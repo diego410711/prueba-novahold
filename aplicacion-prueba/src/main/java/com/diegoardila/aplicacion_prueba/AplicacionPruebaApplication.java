6b351ac5f9a682a1576871a20c4aedb5b6da6130
diego41071 <alexander.diego.2007@hotmail.com>
2025-03-31 01:16:28 -0500

    cambios en configuracion total para ya empezar a desarrollar

--- aplicacion-prueba/src/main/java/com/diegoardila/aplicacion_prueba/AplicacionPruebaApplication.java
+++ aplicacion-prueba/src/main/java/com/diegoardila/aplicacion_prueba/AplicacionPruebaApplication.java
@@ -2,12 +2,11 @@ package com.diegoardila.aplicacion_prueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AplicacionPruebaApplication {
-
+public class AplicacionPruebaApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(AplicacionPruebaApplication.class, args);
 	}
-
 }
