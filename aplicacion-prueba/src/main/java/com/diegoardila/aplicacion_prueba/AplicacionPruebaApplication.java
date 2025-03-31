3c22b09d2544fa3542904845a82731f9126cb52d
diego41071 <alexander.diego.2007@hotmail.com>
2025-03-30 21:31:09 -0500

    primer commit

---
+++ aplicacion-prueba/src/main/java/com/diegoardila/aplicacion_prueba/AplicacionPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.diegoardila.aplicacion_prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionPruebaApplication.class, args);
+	}
+
+}
---
+++ aplicacion-prueba/src/test/java/com/diegoardila/aplicacion_prueba/AplicacionPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoardila.aplicacion_prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
