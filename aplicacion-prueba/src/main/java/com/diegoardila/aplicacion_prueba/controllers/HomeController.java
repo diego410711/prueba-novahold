3bbdfb783a2f567fac840129d6bdff3549c01a5f
diego41071 <alexander.diego.2007@hotmail.com>
2025-03-31 01:37:43 -0500

    configuracion de jsp

---
+++ aplicacion-prueba/src/main/java/com/diegoardila/aplicacion_prueba/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.diegoardila.aplicacion_prueba.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("mensaje", "¡Bienvenido a la aplicación!");
+        return "home"; // Se refiere a home.jsp en WEB-INF/views/
+    }
+}
